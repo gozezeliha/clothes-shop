f1f4772bfc3c099e5218bdf4cc4949a9a5c7585b
Your Name <selimserifoglu03@gmail.com>
2021-05-27 20:54:41 +0300

    project finished

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import ProductsProvider from './context/ProductsProvider';
 import Header from './components/Header';
 import ProductsList from './components/ProductsList';
 import Buy from './components/Buy';
+import Github from './components/Github';
 
 const App = () => {
   return (
 
     <ProductsProvider>
+      <Github/>
       <div className="container">
         <section className="main">
           <Header />
--- src/context/ProductsProvider.js
+++ src/context/ProductsProvider.js
@@ -1,176 +1,184 @@
-import { useState,createContext,useEffect } from 'react';
+import { useState, createContext } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 export const Context = createContext();
 
 const ProductsProvider = (props) => {
 
-    const [products,setProducts] = useState([
-          {
-            id: uuidv4(),
-            title: "Cat Tee Black T-Shirt",
-            image: "https://images.pexels.com/photos/3147528/pexels-photo-3147528.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["S", "XS"],
-            price: 10.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Dark Thug Blue-Navy T-Shirt",
-            image: "https://images.pexels.com/photos/7562315/pexels-photo-7562315.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["M"],
-            price: 29.45
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Sphynx Tie Dye Wine T-Shirt",
-            image: "https://images.pexels.com/photos/2896840/pexels-photo-2896840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["X", "L", "XL"],
-            price: 9.0
-          },
-          {
-            id: uuidv4(),
-            title: "Short Sleeve T-Shirt",
-            image: "https://images.pexels.com/photos/2896853/pexels-photo-2896853.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["XS", "X", "L", "ML", "XL"],
-            price: 75.0
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Sphynx Tie Dye Grey T-Shirt",
-            image: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["X", "L", "XL", "XXL"],
-            price: 10.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Sphynx Tie Dye Grey T-Shirt",
-            image: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["X", "L", "XL", "XXL"],
-            price: 10.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Danger Knife Grey",
-            image: "https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["X", "L"],
-            price: 14.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Sphynx Tie Dye Grey T-Shirt",
-            image: "https://images.pexels.com/photos/1254502/pexels-photo-1254502.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["X", "L"],
-            price: 14.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Short Sleeve T-Shirt",
-            image: "https://images.pexels.com/photos/914472/pexels-photo-914472.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["XL"],
-            price: 25.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Tso 3D Short Sleeve T-Shirt A",
-            image: "https://images.pexels.com/photos/769733/pexels-photo-769733.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["X", "L", "XL"],
-            price: 10.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Short Sleeve T-Shirt",
-            image: "https://images.pexels.com/photos/7873111/pexels-photo-7873111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["XL", "XXL"],
-            price: 49.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Crazy Monkey Black T-Shirt",
-            image: "https://images.pexels.com/photos/670786/pexels-photo-670786.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["S"],
-            price: 22.5
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Short Sleeve T-Shirt",
-            image: "https://images.pexels.com/photos/6315112/pexels-photo-6315112.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["XL"],
-            price: 18.7
-          },
-      
-          {
-            id: uuidv4(),
-            title: "Crazy Monkey Grey",
-            image: "https://images.pexels.com/photos/6311657/pexels-photo-6311657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["L", "XL"],
-            price: 134.9
-          },
-      
-          {
-            id: uuidv4(),
-            title: "On The Streets Black T-Shirt",
-            image: "https://images.pexels.com/photos/7706424/pexels-photo-7706424.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            availableSizes: ["L", "XL"],
-            price: 49.0
-          }
-    ])
+  const [products, setProducts] = useState([
+    {
+      id: uuidv4(),
+      title: "Cat Tee Black T-Shirt",
+      image: "https://images.pexels.com/photos/3147528/pexels-photo-3147528.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["S", "XS"],
+      price: 10.9
+    },
 
-    const [budget,setBudget] = useState([]);
+    {
+      id: uuidv4(),
+      title: "Dark Thug Blue-Navy T-Shirt",
+      image: "https://images.pexels.com/photos/7562315/pexels-photo-7562315.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["M"],
+      price: 29.45
+    },
 
-    const [filters] = useState(["XS","S","M","ML","L","XL","XXL"]);
+    {
+      id: uuidv4(),
+      title: "Sphynx Tie Dye Wine T-Shirt",
+      image: "https://images.pexels.com/photos/2896840/pexels-photo-2896840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["X", "L", "XL"],
+      price: 9.0
+    },
+    {
+      id: uuidv4(),
+      title: "Short Sleeve T-Shirt",
+      image: "https://images.pexels.com/photos/2896853/pexels-photo-2896853.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["XS", "X", "L", "ML", "XL"],
+      price: 75.0
+    },
 
-    const [totalCount,setTotalCount] = useState(0);
+    {
+      id: uuidv4(),
+      title: "Sphynx Tie Dye Grey T-Shirt",
+      image: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["X", "L", "XL", "XXL"],
+      price: 10.9
+    },
 
-    const [isOpen,setIsOpen] = useState(true);
+    {
+      id: uuidv4(),
+      title: "Sphynx Tie Dye Grey T-Shirt",
+      image: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["X", "L", "XL", "XXL"],
+      price: 10.9
+    },
 
-    const addBudget = (product) => {
-        setTotalCount(totalCount => totalCount += product.price);
-        setBudget([...budget,product]);
-        setIsOpen(false);
+    {
+      id: uuidv4(),
+      title: "Danger Knife Grey",
+      image: "https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["X", "L"],
+      price: 14.9
+    },
+
+    {
+      id: uuidv4(),
+      title: "Sphynx Tie Dye Grey T-Shirt",
+      image: "https://images.pexels.com/photos/1254502/pexels-photo-1254502.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["X", "L"],
+      price: 14.9
+    },
+
+    {
+      id: uuidv4(),
+      title: "Short Sleeve T-Shirt",
+      image: "https://images.pexels.com/photos/914472/pexels-photo-914472.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["XL"],
+      price: 25.9
+    },
+
+    {
+      id: uuidv4(),
+      title: "Tso 3D Short Sleeve T-Shirt A",
+      image: "https://images.pexels.com/photos/769733/pexels-photo-769733.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["X", "L", "XL"],
+      price: 10.9
+    },
+
+    {
+      id: uuidv4(),
+      title: "Short Sleeve T-Shirt",
+      image: "https://images.pexels.com/photos/7873111/pexels-photo-7873111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["XL", "XXL"],
+      price: 49.9
+    },
+
+    {
+      id: uuidv4(),
+      title: "Crazy Monkey Black T-Shirt",
+      image: "https://images.pexels.com/photos/670786/pexels-photo-670786.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["S"],
+      price: 22.5
+    },
+
+    {
+      id: uuidv4(),
+      title: "Short Sleeve T-Shirt",
+      image: "https://images.pexels.com/photos/6315112/pexels-photo-6315112.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["XL"],
+      price: 18.7
+    },
+
+    {
+      id: uuidv4(),
+      title: "Crazy Monkey Grey",
+      image: "https://images.pexels.com/photos/6311657/pexels-photo-6311657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["L", "XL"],
+      price: 134.9
+    },
+
+    {
+      id: uuidv4(),
+      title: "On The Streets Black T-Shirt",
+      image: "https://images.pexels.com/photos/7706424/pexels-photo-7706424.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      availableSizes: ["L", "XL"],
+      price: 49.0
     }
+  ])
 
-    const deleteBudget = (id,price) => {
-       setTotalCount(totalCount => totalCount -= price);
-       setBudget(budget.filter(element => element.id !== id));
+  const [budget, setBudget] = useState([]);
+
+  const [filters] = useState(["XS", "S", "M", "ML", "L", "XL", "XXL"]);
+
+  const [totalCount, setTotalCount] = useState(0);
+
+  const [isOpen, setIsOpen] = useState(true);
+
+  const addBudget = (product) => {
+    let isAny = true;
+    budget.map(element => {
+      if(element.id === product.id){
+        isAny = false;
+      }
+    })
+    if(isAny){
+      setBudget([...budget, product]);
+      setTotalCount(totalCount => totalCount += product.price);
+      setIsOpen(false);
     }
+  }
 
-    const filterProducts = (value) => {
-      value === "lowest" ? setProducts([...products].sort((a,b) => a.price - b.price))
-      : setProducts([...products].sort((a,b) => b.price - a.price))
-    }
+  const deleteBudget = (id, price) => {
+    setTotalCount(totalCount => totalCount -= price);
+    setBudget(budget.filter(element => element.id !== id));
+  }
 
-    const changeStatus = (query) => {
-      !query ? setIsOpen(true) : setIsOpen(false);
-    }
+  const filterProducts = (value) => {
+    value === "lowest" ? setProducts([...products].sort((a, b) => a.price - b.price))
+      : setProducts([...products].sort((a, b) => b.price - a.price))
+  }
 
-    return(
-        <Context.Provider value={{
-          products,
-          filters,
-          budget,
-          totalCount,
-          isOpen,
-          addBudget,
-          deleteBudget,
-          filterProducts,
-          changeStatus
-          }}>
+  const changeStatus = (query) => {
+    !query ? setIsOpen(true) : setIsOpen(false);
+  }
 
-            {props.children}
+  return (
+    <Context.Provider value={{
+      products,
+      filters,
+      budget,
+      totalCount,
+      isOpen,
+      addBudget,
+      deleteBudget,
+      filterProducts,
+      changeStatus
+    }}>
 
-        </Context.Provider>
-    )
+      {props.children}
+
+    </Context.Provider>
+  )
 }
 
 export default ProductsProvider;
